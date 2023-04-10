690529b6ef45758801d975d6a6cfdec8809463b6
avr87 <annavanruiten@gmail.com>
2023-04-10 15:34:58 +0100

    added bootstrap, axios, react. Initial push

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+
+export default function App() {
+  return (
+    <div className="container">
+      <div className="App">
+        <header className="App-header">Dictionary</header>
+      </div>
+    </div>
+  );
+}
