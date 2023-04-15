8103da3a4cf00fff9c69066b93ebb1f097112037
avr87 <annavanruiten@gmail.com>
2023-04-15 23:10:59 +0100

    changed title

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <header className="App-header">Dictionary</header>
+        <header className="App-header">English Dictionary</header>
         <main><Search  defaultKeyword="safari" /></main>
         <footer>
           Coded by Anna van Ruiten and hosted on <a href="https://earnest-dusk-24b82f.netlify.app">Netlify</a>
